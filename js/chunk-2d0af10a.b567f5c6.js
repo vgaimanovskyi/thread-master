0abe20a353b89499b94e8faf15f54b99c8810820
vgaimanovskyi <vgaymanovskiy@gmail.com>
2020-08-28 20:17:00 +0300

    new dist commit

--- js/chunk-2d0af10a.b567f5c6.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0af10a"],{"0d77":function(e,n,t){"use strict";t.r(n);var l=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("Gallery")])])}],a={},c=a,u=t("2877"),s=Object(u["a"])(c,l,r,!1,null,null,null);n["default"]=s.exports}}]);
-//# sourceMappingURL=chunk-2d0af10a.b567f5c6.js.map
